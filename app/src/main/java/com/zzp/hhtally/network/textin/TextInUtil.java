ad5cebbbd7a6138dc5148979b37df6011cf3ff23
zzploveandpace <2055984287@qq.com>
2023-04-17 21:19:49 +0800

    淦

--- app/src/main/java/com/zzp/hhtally/network/textin/TextInUtil.java
+++ app/src/main/java/com/zzp/hhtally/network/textin/TextInUtil.java
@@ -11,15 +11,18 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class TextInUtil {
+
+    private static final String appId = "030451c62bb1d44fb2389ad1636cc4b7";
+    private static final String secretCode = "2d34835bf5b9c88d7aa25d71744d461b";
     public static String billsCrop(byte[] imgData)  {
         // 国内通用票据识别
         String url = "https://api.textin.com/robot/v1.0/api/bills_crop";
         // 请登录后前往 “工作台-账号设置-开发者信息” 查看 x-ti-app-id
         // 示例代码中 x-ti-app-id 非真实数据
-        String appId = "030451c62bb1d44fb2389ad1636cc4b7";
+
         // 请登录后前往 “工作台-账号设置-开发者信息” 查看 x-ti-secret-code
         // 示例代码中 x-ti-secret-code 非真实数据
-        String secretCode = "2d34835bf5b9c88d7aa25d71744d461b";
+
         BufferedReader in = null;
         DataOutputStream out = null;
         StringBuilder result = new StringBuilder();
@@ -60,6 +63,107 @@ public class TextInUtil {
             }
         }
         return result.toString();
+    }
+
+
+    public static String imgCropEnhance(byte[] imgData) {
+        // 图片切边增强
+        String url = "https://api.textin.com/ai/service/v1/crop_enhance_image";
+        // 请登录后前往 “工作台-账号设置-开发者信息” 查看 x-ti-app-id
+        // 示例代码中 x-ti-app-id 非真实数据
+
+        // 请登录后前往 “工作台-账号设置-开发者信息” 查看 x-ti-secret-code
+        // 示例代码中 x-ti-secret-code 非真实数据
+
+        BufferedReader in = null;
+        DataOutputStream out = null;
+        StringBuilder result = new StringBuilder();
+        try {
+            URL realUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection)realUrl.openConnection();
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type", "application/octet-stream");
+            conn.setRequestProperty("x-ti-app-id", appId);
+            conn.setRequestProperty("x-ti-secret-code", secretCode);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST"); // 设置请求方式
+            out = new DataOutputStream(conn.getOutputStream());
+            out.write(imgData);
+            out.flush();
+            out.close();
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (Exception e) {
+            System.out.println("发送 POST 请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return result.toString();
+    }
+
+    public static String imgDewarp(byte[] imgData) {
+        String url = "https://api.textin.com/ai/service/v1/dewarp";
+        // 请登录后前往 “工作台-账号设置-开发者信息” 查看 x-ti-app-id
+        // 示例代码中 x-ti-app-id 非真实数据
+        // 请登录后前往 “工作台-账号设置-开发者信息” 查看 x-ti-secret-code
+        // 示例代码中 x-ti-secret-code 非真实数据
+        BufferedReader in = null;
+        DataOutputStream out = null;
+        StringBuilder result = new StringBuilder();
+        try {
+
+            URL realUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection)realUrl.openConnection();
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type", "application/octet-stream");
+            conn.setRequestProperty("x-ti-app-id", appId);
+            conn.setRequestProperty("x-ti-secret-code", secretCode);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST"); // 设置请求方式
+            out = new DataOutputStream(conn.getOutputStream());
+            out.write(imgData);
+            out.flush();
+            out.close();
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (Exception e) {
+            System.out.println("发送 POST 请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return result.toString().toString();
     }
 
     public static byte[] readFile(String path)
--- app/src/main/java/com/zzp/hhtally/ui/receipt/add/AddReceiptPresenter.kt
+++ app/src/main/java/com/zzp/hhtally/ui/receipt/add/AddReceiptPresenter.kt
@@ -10,9 +10,7 @@ import com.zzp.hhtally.data.Label
 import com.zzp.hhtally.network.HttpCallback
 import com.zzp.hhtally.network.HttpResult
 import com.zzp.hhtally.network.RetrofitManager
-import com.zzp.hhtally.network.textin.BillsCropData
-import com.zzp.hhtally.network.textin.TextInResult
-import com.zzp.hhtally.network.textin.TextInUtil
+import com.zzp.hhtally.network.textin.*
 import com.zzp.hhtally.util.*
 import kotlin.concurrent.thread
 
@@ -41,100 +39,139 @@ class AddReceiptPresenter(baseView: IAddReceiptView) : BasePresenter<IAddReceipt
         val gson = Gson()
         view.showLoadingDialog()
         thread {
-            val type = object : TypeToken<TextInResult<BillsCropData>>() {}.type
 
-            val result: TextInResult<BillsCropData> =
-                gson.fromJson(TextInUtil.billsCrop(bitmap2ByteArray(image)), type)
-//            result.toString().logD()
-            activity.runOnUiThread {
-                if (result.code == 200) {
-                    view.dismissLoadingDialog()
-                    if (result.result.object_list.isNotEmpty()) {
-                        var billPrice = ""
-                        var billTime = ""
-                        var billType = ""
-                        var billShopkeeper = ""
-                        result.result.object_list[0].apply {
-                            billType = this.kind_description
+            val typeDewarpData = object : TypeToken<TextInResult<DewarpData>>() {}.type
 
-                            if (!LabelUtil.isContainLabelName(billType)) {
-                                RetrofitManager.apiService.addLabel(billType).execute(
-                                    activity.bindToLifecycle(),
-                                    object : HttpCallback<HttpResult<Int>>() {
-                                        override fun onSuccess(model: HttpResult<Int>) {
-                                            if (model.code == 200) {
-                                                LabelUtil.labelList.add(
-                                                    Label(
-                                                        -1,
-                                                        model.data,
-                                                        billType,
-                                                        null
-                                                    )
-                                                )
-                                                view.refreshLabelPicker(LabelUtil.labelList)
-                                            } else {
-                                                model.msg.showToast()
-                                                billType = ""
+            val resultDewarp: TextInResult<DewarpData> =
+                gson.fromJson(TextInUtil.imgDewarp(bitmap2ByteArray(image)), typeDewarpData)
+
+            if (resultDewarp.code == 200) {
+                val typeCropEnhanceData =
+                    object : TypeToken<TextInResult<CropEnhanceData>>() {}.type
+
+                val resultCropEnhance: TextInResult<CropEnhanceData> = gson.fromJson(
+                    TextInUtil.imgCropEnhance(
+                        base642ByteArray(resultDewarp.result.image)
+                    ), typeCropEnhanceData
+                )
+
+                if (resultCropEnhance.code == 200) {
+                    val typeBillsCropData =
+                        object : TypeToken<TextInResult<BillsCropData>>() {}.type
+
+                    val result: TextInResult<BillsCropData> =
+                        gson.fromJson(
+                            TextInUtil.billsCrop(base642ByteArray(resultCropEnhance.result.image_list[0].image)),
+                            typeBillsCropData
+                        )
+                    activity.runOnUiThread {
+                        if (result.code == 200) {
+                            view.dismissLoadingDialog()
+                            if (result.result.object_list.isNotEmpty()) {
+                                var billPrice = ""
+                                var billTime = ""
+                                var billType = ""
+                                var billShopkeeper = ""
+                                result.result.object_list[0].apply {
+                                    billType = this.kind_description
+
+                                    if (!LabelUtil.isContainLabelName(billType)) {
+                                        RetrofitManager.apiService.addLabel(billType).execute(
+                                            activity.bindToLifecycle(),
+                                            object : HttpCallback<HttpResult<Int>>() {
+                                                override fun onSuccess(model: HttpResult<Int>) {
+                                                    if (model.code == 200) {
+                                                        LabelUtil.labelList.add(
+                                                            Label(
+                                                                -1,
+                                                                model.data,
+                                                                billType,
+                                                                null
+                                                            )
+                                                        )
+                                                        view.refreshLabelPicker(LabelUtil.labelList)
+                                                    } else {
+                                                        model.msg.showToast()
+                                                        billType = ""
+                                                    }
+                                                }
+
+                                            })
+                                    }
+
+                                    when (this.type) {
+                                        //火车票
+                                        "train_ticket" -> {
+                                            for (item in this.item_list) {
+                                                when (item.key) {
+                                                    "price" -> billPrice = item.value
+                                                    "departure_date" -> billTime = item.value
+                                                }
+                                            }
+
+                                        }
+                                        //商户小票
+                                        "shop_receipt" -> {
+                                            for (item in this.item_list) {
+                                                when (item.key) {
+                                                    "money" -> billPrice = item.value
+                                                    "date" -> billTime = item.value
+                                                    "shop" -> billShopkeeper = item.value
+                                                }
                                             }
                                         }
-
-                                    })
-                            }
-
-                            when (this.type) {
-                                //火车票
-                                "train_ticket" -> {
-                                    for (item in this.item_list) {
-                                        when (item.key) {
-                                            "price" -> billPrice = item.value
-                                            "departure_date" -> billTime = item.value
+                                        //医疗费收据
+                                        "medical_receipt" -> {
+                                            for (item in this.item_list) {
+                                                when (item.key) {
+                                                    "amount_small" -> billPrice = item.value
+                                                }
+                                            }
+                                        }
+                                        //出租车发票
+                                        "taxi_ticket" -> {
+                                            for (item in this.item_list) {
+                                                when (item.key) {
+                                                    "sum" -> billPrice = item.value
+                                                    "date" -> billTime = item.value
+                                                }
+                                            }
+                                        }
+                                        //增值税普通发票
+                                        "vat_special_invoice", "vat_electronic_special_invoice", "vat_electronic_invoice", "vat_common_invoice", "vat_electronic_toll_invoice", "machine_printed_invoice", "blockchain_electronic_invoice", "vat_electronic_invoice_new", "vat_electronic_special_invoice_new" -> {
+                                            for (item in this.item_list) {
+                                                when (item.key) {
+                                                    "vat_invoice_price_list" -> billPrice =
+                                                        item.value
+                                                    "vat_invoice_issue_date" -> billTime =
+                                                        item.value
+                                                    "vat_invoice_seller_name" -> billShopkeeper =
+                                                        item.value
+                                                }
+                                            }
                                         }
                                     }
-
-                                }
-                                //商户小票
-                                "shop_receipt" -> {
-                                    for (item in this.item_list) {
-                                        when (item.key) {
-                                            "money" -> billPrice = item.value
-                                            "date" -> billTime = item.value
-                                            "shop" -> billShopkeeper = item.value
-                                        }
-                                    }
-                                }
-                                //医疗费收据
-                                "medical_receipt" -> {
-                                    for (item in this.item_list) {
-                                        when (item.key) {
-                                            "amount_small" -> billPrice = item.value
-                                        }
-                                    }
-                                }
-                                //出租车发票
-                                "taxi_ticket" -> {
-                                    for (item in this.item_list) {
-                                        when (item.key) {
-                                            "sum" -> billPrice = item.value
-                                            "date" -> billTime = item.value
-                                        }
-                                    }
-                                }
-                                //增值税普通发票
-                                "vat_special_invoice", "vat_electronic_special_invoice", "vat_electronic_invoice", "vat_common_invoice", "vat_electronic_toll_invoice", "machine_printed_invoice", "blockchain_electronic_invoice", "vat_electronic_invoice_new", "vat_electronic_special_invoice_new" -> {
-                                    for (item in this.item_list) {
-                                        when (item.key) {
-                                            "vat_invoice_price_list" -> billPrice = item.value
-                                            "vat_invoice_issue_date" -> billTime = item.value
-                                            "vat_invoice_seller_name" -> billShopkeeper = item.value
-                                        }
-                                    }
+                                    view.refreshFromImg(
+                                        billPrice,
+                                        billTime,
+                                        billType,
+                                        billShopkeeper
+                                    )
                                 }
                             }
-                            view.refreshFromImg(billPrice, billTime, billType, billShopkeeper)
+                        } else {
+                            result.message.showToast()
                         }
                     }
                 } else {
-                    result.message.showToast()
+                    activity.runOnUiThread {
+                        resultCropEnhance.message.showToast()
+                    }
+                }
+
+            } else {
+                activity.runOnUiThread {
+                    resultDewarp.message.showToast()
                 }
             }
         }
